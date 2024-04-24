62e0b237b910db545666674861c345c8b1eaff25
SHEFF48 <sun-service@ukr.net>
2024-04-24 17:01:09 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "h-screen w-full  bg-background font-sans antialiased ",
+          "h-svh w-full  bg-background font-sans antialiased ",
           fontSans.variable
         )}
       >
