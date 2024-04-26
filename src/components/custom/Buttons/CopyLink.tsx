1b4dbbd38a6cdca372e12c9cc4f7271c4cd668b7
SHEFF48 <sun-service@ukr.net>
2024-04-26 08:19:32 +0300

    add toolptip

--- src/components/custom/Buttons/CopyLink.tsx
+++ src/components/custom/Buttons/CopyLink.tsx
@@ -4,6 +4,13 @@ import React, { FC, useState } from "react";
 import { toast } from "sonner";
 import CopyIcon from "@/components/custom/menu-icons/Copy";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 interface ICopyLink {
   link?: string;
   visibility?: boolean;
@@ -20,20 +27,31 @@ const CopyLink: FC<ICopyLink> = ({
     navigator.clipboard.writeText(link);
     toast(` Link copied.`);
     setCopyStatus((prev) => !prev);
+
+    setTimeout(() => {
+      setCopyStatus(false);
+    }, 2000);
   };
 
   return (
     <>
       {variant === "page" && (
-        <div
-          className="flex justify-between items-center gap-2 cursor-pointer h-[40px] w-full px-2 rounded-[10px] bg-white shadow-[0_1px_10px_0_rgba(32,_32,_32,_0.15)] "
-          onClick={clickHandler}
-        >
-          <div className="text-[14px]">
-            {!copyStatus ? link : "Посилання скопійовано"}
-          </div>
-          <CopyIcon />
-        </div>
+        <TooltipProvider disableHoverableContent>
+          <Tooltip open={copyStatus}>
+            <TooltipTrigger asChild>
+              <div
+                className="flex justify-between items-center gap-2 cursor-pointer h-[40px] w-full px-2 rounded-[10px] bg-white shadow-[0_1px_10px_0_rgba(32,_32,_32,_0.15)] "
+                onClick={clickHandler}
+              >
+                <div className="text-[14px]">{link}</div>
+                <CopyIcon />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent hideWhenDetached={true} side="bottom" align="end">
+              <p>Посилання скопійовано</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       )}
       {variant === "tooltip" && (
         <div
